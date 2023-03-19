0f9a07ebe7fd69d4d14fca5995729385e20442b0
kahvia-d <xuedongtan@163.com>
2023-03-19 14:15:18 +0800

    修正了判断”确认跳过“的逻辑，现在可以正常跳过导语和章节测验。 添加了视频中题目的检测和回答。

--- autoNextVideoChaoXing.js
+++ autoNextVideoChaoXing.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         myself
+// @name         阿东的脚本
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -19,10 +19,18 @@ var secondIframeDocument
 var videoButton
 var video
 
+
+function playVideo() {
+    var hs = secondIframeDocument.getElementsByClassName('vjs-icon-placeholder')
+    let model = document.createElement("button")
+    model.click
+    hs[0].click()
+}
+
 function tryVideo() {
     //找到顶级文档
     topDoc = document
-    // console.log(document.getElementsByTagName('iframe'));
+    // console.log("顶级文档下面的iframe层:" + document.getElementsByTagName('iframe'));
     //查询它下面的的iframe层
     iframes1 = document.getElementsByTagName('iframe')
     if (iframes1 != undefined) {
@@ -45,15 +53,14 @@ function tryVideo() {
                         secondIframeDocument = secondIframe.contentDocument
                         if (secondIframeDocument != undefined) {
                             video = secondIframeDocument.getElementById('video')
+                            console.log("video:" + video)
                             if (video != null) {
                                 //通过class标签分析对应作用，started为去除遮罩
+                                topDoc.getElementsByClassName('prev_title')[0].click()
                                 video.className += " vjs-has-started"
                                 // console.log("click")
                                 //经过反复测试，只能通过模拟点击遮罩去除后，表层的播放按钮，使其自动播放。需要用户在页面加载后手动点击一下空白处，才能使模拟点击生效。
-                                var hs = secondIframeDocument.getElementsByClassName('vjs-icon-placeholder')
-                                let model = document.createElement("button")
-                                model.click
-                                hs[0].click()
+                                playVideo()
                             }
                         }
                     }
@@ -63,10 +70,15 @@ function tryVideo() {
     }
 }
 
+
+
 //尝试跳转下一节，测验自动跳过。
 function tryNext() {
     var checker = setInterval(function () {
-        video = secondIframeDocument.getElementById('video')
+        if (secondIframeDocument != undefined) {
+            video = secondIframeDocument.getElementById('video')
+        }
+
         let className
         if (video != null) {
             className = video.className;
@@ -78,19 +90,37 @@ function tryNext() {
                     let tempButton = document.createElement("button")
                     tempButton.onclick = button2.onclick
                     tempButton.click()
-                    console.log(button2)
-                    console.log(button2.onclick);
-                    // let button3 = topDoc.getElementsByClassName("nextChapter")[0]
-                    // if (button3 != null) {
-                    //     tempButton.onclick = button3.onclick
-                    //     tempButton.click()
-                    // }
+                    // console.log(button2)
+                    // console.log(button2.onclick);
                 }
-                console.log("running")
+                console.log("播放结束")
                 //结束这一节时，开启新轮回，判断是否需要播放视频
                 oneTime()
                 //清楚当前这个用完了的定时器
                 clearInterval(checker)
+            }
+            if (className != null && className.includes('vjs-paused')) {
+                //检测到暂停了就让它继续播放
+                playVideo()
+                console.log("尝试检测题目")
+                var options = secondIframeDocument.getElementsByClassName('tkRadio');
+                var subButton = secondIframeDocument.getElementById('videoquiz-submit');
+                console.log(options);
+                console.log(subButton);
+                try {
+                    for (var i = 0; i < options.length; i++) {
+                        console.log("尝试选项:" + i)
+                        options[i].click()
+                        subButton.click()
+                        if (i == options.length - 1) {
+                            //正儿八经最后一个才是正确的
+                            console.log("回答完毕")
+                        }
+                    }
+                } catch (e) {
+                    //正确选项不是最后一个，若是选到正确的，选项框就会消失，再次顺序提交会捕获异常
+                    console.log("回答完毕")
+                }
             }
         } else {
             //没在播放视频，则模拟点击下一节，跳过答题，同时通过模拟跳过确认
@@ -99,14 +129,20 @@ function tryNext() {
                 let tempButton = document.createElement("button")
                 tempButton.onclick = button2.onclick
                 tempButton.click()
+                console.log("下一章")
                 let button3 = topDoc.getElementsByClassName("nextChapter")[0]
-                if (button3 != null) {
+                console.log(topDoc.getElementsByClassName("nextChapter")[0])
+                console.log("按钮状态display:" + button3.style.diplay)
+                //这个按钮要是隐藏的，就说明这按钮是跳过测验时要触发的按钮，现在只是跳过开头介绍，不需要触发
+                console.log("style.diplay:" + button3.style.display)
+                if (button3 != null && (button3.style.display == "")) {
                     tempButton.onclick = button3.onclick
                     tempButton.click()
+                    console.log("确认跳过答题，去下一章")
                     video = document.createElement("div")
-                    oneTime()
-                    clearInterval(checker)
                 }
+                oneTime()
+                clearInterval(checker)
             }
         }
     }, 1000)
@@ -114,6 +150,7 @@ function tryNext() {
 
 function oneTime() {
     setTimeout(() => {
+        document.createElement("test").click()
         tryVideo()
         tryNext()
     }, 3000)
